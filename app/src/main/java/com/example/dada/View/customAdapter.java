6fcc046fb4babe878e2c82cf054ab892cecf2705
KevinHQi <klqzpro@gmail.com>
2018-04-06 23:09:25 -0600

    add ugly colored listView

--- app/src/main/java/com/example/dada/View/customAdapter.java
+++ app/src/main/java/com/example/dada/View/customAdapter.java
@@ -1,23 +1,17 @@
 package com.example.dada.View;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-
-
-import com.example.dada.Controller.TaskController;
-import com.example.dada.Model.OnAsyncTaskCompleted;
 import com.example.dada.Model.Task.Task;
 import com.example.dada.R;
-import com.example.dada.Util.FileIOUtil;
-
 import java.util.ArrayList;
-import java.util.List;
+
 
 public class customAdapter extends ArrayAdapter<Task>{
     private Context mContext;
@@ -37,12 +31,33 @@ public class customAdapter extends ArrayAdapter<Task>{
         String title = getItem(position).getTitle();
         String status = getItem(position).getStatus();
 
+
+
         LayoutInflater inflater = LayoutInflater.from(mContext);
         convertView = inflater.inflate(mResource, parent, false);
 
         TextView taskTitle = (TextView) convertView.findViewById(R.id.taskTitle_requester_main);
         TextView taskStatus = (TextView) convertView.findViewById(R.id.taskStatus_requester_main);
 
+        taskStatus.setTextColor(Color.parseColor("white"));
+        if (status.equals("requested")){
+            taskStatus.setBackgroundColor(Color.parseColor("red"));
+        }
+        else if (status.equals("bidded")){
+            taskStatus.setBackgroundColor(Color.parseColor("blue"));
+        }
+        else if (status.equals("assigned")){
+            taskStatus.setBackgroundColor(Color.parseColor("green"));
+        }
+        else if (status.equals("completed")){
+            taskStatus.setBackgroundColor(Color.parseColor("cyan"));
+        }
+        else if (status.equals("done")){
+            taskStatus.setBackgroundColor(Color.parseColor("darkgray"));
+        }
+
+
+//        taskStatus.setTextColor(Color.parseColor("#fe00fb"));
         taskTitle.setText(title);
         taskStatus.setText(status);
 
